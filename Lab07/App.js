07e0d87a5a8f16daf793b53aab87d0edaa07e729
nguyenquoctrung2 <nguyenquoctrung020525@gmail.com>
2023-10-20 08:56:39 +0700

    Layout xe dap 1

---
+++ Lab07/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView,Image,Button} from 'react-native';
+import Layout1 from './View/Layout1';
+
+export default function App() {
+  return (
+    <Layout1/>
+  )
+}
